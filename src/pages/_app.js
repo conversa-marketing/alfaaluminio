e62ca006c0e49e273a565078f79b4dc18be3afb7
Gabriel Hercules <gabrielsyze@gmail.com>
2021-11-23 16:53:50 -0300

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,33 +1,11 @@
 import '../../styles/globals.css'
-import Script from 'next/script';
 import Layout from '../components/Layout'
 
-import Head from 'next/head'
 
 function App({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-J28NXFKZ29`}
-      />
-      <Script strategy="lazyOnload">
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-J28NXFKZ29');
-            `}
-      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -27,4 +27,50 @@ export default class MyDocument extends Document {
             sheet.seal()
         }
     }
+
+    render() {
+        const GA_MEASUREMENT_ID = 'GA_MEASUREMENT_ID'; // Paste your GTAG here
+        return (
+            <Html lang="pt_BR">
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap"
+                        rel="stylesheet"
+                    />
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                    />
+                    <script
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_MEASUREMENT_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+
+    // return (
+    //     <>
+    // <Head>
+    //     <link
+    //       href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap"
+    //       rel="stylesheet"
+    //     />
+    //   </Head>
+    //   </>
+    //     )
 }
